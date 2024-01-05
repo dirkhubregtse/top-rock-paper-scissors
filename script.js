a29f75c44a34ebc4fef4c87887e7c0e1cda8f86c
Dirk  Hubregtse <20535135+dirkhubregtse@users.noreply.github.com>
2024-01-05 09:51:11 +0100

    Deploy the layout

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function onHoverRanged()
+{
+    document.getElementById("ranged").src="./images/ranged-selected.png";
+}
+
+function offHoverRanged()
+{
+    document.getElementById("ranged").src="./images/ranged.png";
+}
+
+function onHoverMelee()
+{
+    document.getElementById("melee").src="./images/melee-selected.png";
+}
+
+function offHoverMelee()
+{
+    document.getElementById("melee").src="./images/melee.png";
+}
+
+function onHoverFlying()
+{
+    document.getElementById("flying").src="./images/flying-selected.png";
+}
+
+function offHoverFlying()
+{
+    document.getElementById("flying").src="./images/flying.png";
+}
